2383cbfb661660a9c6f79b4667b8001ad7a5927f
E-R-J-R <71446505+E-R-J-R@users.noreply.github.com>
2020-09-17 23:27:25 +0800

    Add Web Client Project

---
+++ PokerHandShowdown/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokerHandShowdown.MvcApplication" Language="C#" %>
---
+++ PokerHandShowdown/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokerHandShowdown.Startup))]
+namespace PokerHandShowdown
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
